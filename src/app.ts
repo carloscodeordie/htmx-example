7c7c0a097c398fa481271eef64e3b1f0c80f13fc
Carlos Macias <lmaciasgonzalez@gmail.com>
2025-08-22 18:27:23 -0600

    feat(info): allow users to get information details in multiple ways

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
