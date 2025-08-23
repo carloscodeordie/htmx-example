157e5df09088fdd4242a62cfda792c1164eecfd9
Carlos Macias <lmaciasgonzalez@gmail.com>
2025-08-23 07:35:59 -0600

    feat(notes): allow users to create notes

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,43 @@
 import express from "express";
+import { NOTES } from "./utils/constants";
 const path = require("path");
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/info", (req, res) => {
-  const infoList: string[] = [
-    "This is an example",
-    "About how to display",
-    "An array",
-  ];
+app.get("/notes/new", (req, res) => {
+  res.render("notes");
+});
+
+app.get("/notes", (req, res) => {
+  res.send(`
+    <ul>
+      ${NOTES.map((note) => `<li>${note}</li>`).join("")}
+    </ul>
+  `);
+});
+
+app.post("/notes", (req, res) => {
+  const { note } = req.body;
+
+  // Save notes on memory
+  NOTES.push(note);
 
   res.send(`
     <ul>
-      ${infoList.map((info) => `<li>${info}</li>`).join("")}
+      ${NOTES.map((note) => `<li>${note}</li>`).join("")}
     </ul>
   `);
 });
