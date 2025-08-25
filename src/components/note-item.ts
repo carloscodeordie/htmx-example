313bb62b37d39e3f8888b4b988a857278f94c0cf
Carlos Macias <lmaciasgonzalez@gmail.com>
2025-08-25 11:29:49 -0600

    fix(notes): use advanced selectors to improve the note item creation

--- src/components/note-item.ts
+++ src/components/note-item.ts
@@ -1,12 +1,15 @@
 import { Note } from "../models/interfaces";
 
 export function createNoteItem(note: Note) {
-  return `<div class="notes-list-item" id="note-${note.id}">
-          <span>${note.value}</span>
-          <button
-            hx-delete="/notes/${note.id}"
-            hx-target="#note-${note.id}"
-            hx-swap="outerHTML"
-          >Delete</button>
-        </div>`;
+  return `
+    <div class="notes-list-item" id="note-${note.id}">
+      <span>${note.value}</span>
+      <button
+        hx-delete="/notes/${note.id}"
+        hx-target="closest .notes-list-item"
+        hx-swap="outerHTML"
+      >
+        Delete
+      </button>
+    </div>`;
 }
