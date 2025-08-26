f5b569ea355d5d4ee797d6c03518528d481bb505
Carlos Macias <lmaciasgonzalez@gmail.com>
2025-08-26 15:23:42 -0600

    feat(locations): allows users to add locations to dream locations

--- src/components/locationItem.ts
+++ src/components/locationItem.ts
@@ -3,7 +3,7 @@ import { Location } from "../models/interfaces";
 export default function renderLocationItem(location: Location) {
   return `
     <li class="location-item">
-      <button>
+      <button hx-post="/places" hx-vals='{ "locationId": "${location.id}" }'>
         <img src="${`/images/${location.image.src}`}" alt="${
     location.image.alt
   }" />
